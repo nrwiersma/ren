60d338810fe69d38531bce332f2595beafe599f3
Nicholas Wiersma <nick@wiersma.co.za>
2022-03-19 17:18:53 +0200

    feat: refactor application

--- Makefile
+++ Makefile
@@ -1,4 +1,35 @@
-include github.com/hamba/make/golang
+# Run CI tasks
+ci: lint build test
+.PHONY: ci
+
+# Format all files
+fmt:
+	@echo "==> Formatting source"
+	@gofmt -s -w $(shell find . -type f -name '*.go' -not -path "./vendor/*")
+	@echo "==> Done"
+.PHONY: fmt
+
+# Tidy the go.mod file
+tidy:
+	@echo "==> Cleaning go.mod"
+	@go mod tidy
+	@echo "==> Done"
+.PHONY: tidy
+
+# Build the commands
+build:
+	@find ./cmd/* -maxdepth 1 -type d -exec go build {} \;
+.PHONY: build
+
+# Run all tests
+test:
+	@go test -cover -race ./...
+.PHONY: test
+
+# Lint the project
+lint:
+	@golangci-lint run --go 1.18 ./...
+.PHONY: lint
 
 # Build the docker image
 docker:
--- cmd/ren/server.go
+++ cmd/ren/server.go
@@ -2,64 +2,66 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/hamba/cmd/v2"
-	"github.com/hamba/cmd/v2/term"
 	logCtx "github.com/hamba/logger/v2/ctx"
-	"github.com/hamba/pkg/v2/http"
+	httpx "github.com/hamba/pkg/v2/http"
 	"github.com/nrwiersma/ren/api"
 	"github.com/urfave/cli/v2"
 	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 )
 
-func runServer(_ term.Term) func(c *cli.Context) error {
-	return func(c *cli.Context) error {
-		ctx := c.Context
+func runServer(c *cli.Context) error {
+	ctx := c.Context
 
-		log, err := cmd.NewLogger(c)
-		if err != nil {
-			return err
-		}
-
-		stats, err := cmd.NewStatter(c, log)
-		if err != nil {
-			return err
-		}
-
-		tracer, err := cmd.NewTracer(c, log,
-			semconv.ServiceNameKey.String("ren"),
-			semconv.ServiceVersionKey.String(version),
-		)
-		if err != nil {
-			return err
-		}
-		defer func() { _ = tracer.Shutdown(context.Background()) }()
-
-		app, err := newApplication(c, log, stats, tracer)
-		if err != nil {
-			return err
-		}
-
-		apiHdlr := api.New(app, log, stats, tracer.Tracer("api"))
-
-		port := c.String(cmd.FlagPort)
-		srv := http.NewServer(ctx, ":"+port, apiHdlr)
-
-		log.Info("Starting server", logCtx.Str("port", port))
-		srv.Serve(func(err error) {
-			log.Error("Server error", logCtx.Error("error", err))
-		})
-		defer func() { _ = srv.Close() }()
-
-		<-ctx.Done()
-
-		log.Info("Shutting down")
-
-		if err = srv.Shutdown(10 * time.Second); err != nil {
-			log.Warn("Failed to shutdown server", logCtx.Error("error", err))
-		}
-
-		return nil
+	log, err := cmd.NewLogger(c)
+	if err != nil {
+		return err
 	}
+
+	stats, err := cmd.NewStatter(c, log)
+	if err != nil {
+		return err
+	}
+
+	tracer, err := cmd.NewTracer(c, log,
+		semconv.ServiceNameKey.String("ren"),
+		semconv.ServiceVersionKey.String(version),
+	)
+	if err != nil {
+		return err
+	}
+	defer func() { _ = tracer.Shutdown(context.Background()) }()
+
+	app, err := newApplication(c, log, stats, tracer)
+	if err != nil {
+		return err
+	}
+
+	apiSrv := api.New(app, log, stats, tracer.Tracer("api"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/readyz", http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {}))
+	mux.Handle("/healthz", httpx.NewHealthHandler(app))
+	mux.Handle("/", apiSrv)
+
+	port := c.String(cmd.FlagPort)
+	srv := httpx.NewServer(ctx, ":"+port, mux)
+
+	log.Info("Starting server", logCtx.Str("port", port))
+	srv.Serve(func(err error) {
+		log.Error("Server error", logCtx.Error("error", err))
+	})
+	defer func() { _ = srv.Close() }()
+
+	<-ctx.Done()
+
+	log.Info("Shutting down")
+	if err = srv.Shutdown(10 * time.Second); err != nil {
+		log.Error("Failed to shutdown server", logCtx.Error("error", err))
+	}
+
+	return nil
 }
